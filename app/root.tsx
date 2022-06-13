7de3f11ec11f258a763aa14ada4ce254edc7ecc4
Julian Jark <juliuxu@gmail.com>
2022-06-13 20:29:26 +0200

    :construction: add static menu items to header and footer

--- app/root.tsx
+++ app/root.tsx
@@ -27,14 +27,11 @@ export default function App() {
         <Links />
       </head>
       <body className="flex flex-col h-screen">
-        <Todo
-          title="HEADER"
-          className="border-l-0 border-r-0 border-t-0 border-b-2 h-20"
-        />
+        <Header />
         <main className="flex-grow">
           <Outlet />
         </main>
-        <Todo title="FOOTER" className="bg-red-500 grow border-none h-96" />
+        <Footer />
 
         <ScrollRestoration />
         <Scripts />
@@ -43,3 +40,51 @@ export default function App() {
     </html>
   );
 }
+
+interface MenuItem {
+  title: string;
+  href: string;
+}
+const MENU_ITEMS: MenuItem[] = [
+  { title: "Dette kan vi", href: "/dette-kan-vi" },
+  { title: "Dette har vi gjort", href: "/dette-har-vi-gjort" },
+  { title: "Blogg", href: "/blogg" },
+  { title: "Mentorprogram", href: "/mentor" },
+  { title: "Bli en av oss", href: "/bli-en-av-oss" },
+  { title: "Partnere", href: "/partnere" },
+  { title: "Om oss", href: "/om-oss" },
+  { title: "Ansatte", href: "/ansatte" },
+];
+const Header = () => {
+  return (
+    <Todo
+      title=""
+      className="border-l-0 border-r-0 border-t-0 border-b-2 h-20 w-full"
+    >
+      <div className="flex w-screen gap-2 justify-between items-center px-4">
+        <div className="text-2xl font-bold">CAPRA</div>
+        <ul className="flex items-center">
+          {MENU_ITEMS.map((item) => (
+            <li key={item.href}>
+              <a href={item.href}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Todo>
+  );
+};
+
+const Footer = () => {
+  return (
+    <Todo title="FOOTER" className="bg-red-500 grow border-none h-96">
+      <ul>
+        {MENU_ITEMS.map((item) => (
+          <li key={item.href}>
+            <a href={item.href}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Todo>
+  );
+};
