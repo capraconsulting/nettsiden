56d3cd934f14ec4795d37bf24afbc7537a6f9bb4
Julian Jark <juliuxu@gmail.com>
2022-06-17 17:06:27 +0200

    :adhesive_bandage: set correct offset for contact-form

--- app/components/contact-form.tsx
+++ app/components/contact-form.tsx
@@ -5,7 +5,7 @@ export const ContactForm = () => {
     <Todo
       badge
       title="Contact form"
-      className="bg-blue-700 text-white h-96 w-full -mb-8"
+      className="bg-blue-700 text-white h-96 w-full -mb-[50px]"
     >
       <div className="text-3xl font-semibold text-orange-200">
         Vil du vite mer om hvordan vi kan hjelpe deg?
