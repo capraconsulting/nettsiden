7f929a81cda05d50c974725c21e5faf60f19d3bd
Julian Jark <juliuxu@gmail.com>
2022-06-17 23:07:33 +0200

    :recycle: move call-to-action-box into it's own file

---
+++ app/components/call-to-action-box.tsx
@@ -0,0 +1,22 @@
+import { Todo } from "~/components/todo";
+
+interface CallToActionBoxProps {
+  title: React.ReactNode;
+  description: React.ReactNode;
+  linkText: React.ReactNode;
+}
+export const CallToActionBox = ({
+  title,
+  description,
+  linkText,
+}: CallToActionBoxProps) => {
+  return (
+    <Todo badge title="" className="border-none py-0 px-0 w-full">
+      <div className="w-11/12 max-w-7xl py-24 px-6 bg-orange-100 flex flex-col items-center">
+        <p className="font-bold text-xl">{title}</p>
+        <p>{description}</p>
+        <Todo className="mt-7 w-48" badge size="small" title={linkText} />
+      </div>
+    </Todo>
+  );
+};
--- app/routes/dette-har-vi-gjort/index.tsx
+++ app/routes/dette-har-vi-gjort/index.tsx
@@ -4,6 +4,7 @@ import { Link, useLoaderData } from "@remix-run/react";
 
 import { Todo } from "~/components/todo";
 import sanityClient from "~/sanity/sanity-client.server";
+import { CallToActionBox } from "../../components/call-to-action-box";
 import { TitleAndText } from "../../components/title-and-text";
 
 // TODO: I propose to move this function to a shared utility/common file
@@ -96,24 +97,4 @@ export default function DetteHarViGjort() {
 interface CardGridProps {}
 export const CardGrid = ({}: CardGridProps) => {
   return <Todo />;
-};
-interface CallToActionBoxProps {
-  title: React.ReactNode;
-  description: React.ReactNode;
-  linkText: React.ReactNode;
-}
-export const CallToActionBox = ({
-  title,
-  description,
-  linkText,
-}: CallToActionBoxProps) => {
-  return (
-    <Todo badge title="" className="border-none py-0 px-0 w-full">
-      <div className="w-11/12 max-w-7xl py-24 px-6 bg-orange-100 flex flex-col items-center">
-        <p className="font-bold text-xl">{title}</p>
-        <p>{description}</p>
-        <Todo className="mt-7 w-48" badge size="small" title={linkText} />
-      </div>
-    </Todo>
-  );
 };
