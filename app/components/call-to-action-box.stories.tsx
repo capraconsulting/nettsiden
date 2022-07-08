a8138d624f1e81766ba056808766f7d30d81745f
Julian Jark <juliuxu@gmail.com>
2022-07-08 14:13:36 +0200

    :technologist: setup ladle for component development

---
+++ app/components/call-to-action-box.stories.tsx
@@ -0,0 +1,8 @@
+import { CallToActionBox } from "./call-to-action-box";
+export const CallToActionBoxStories = () => (
+  <CallToActionBox
+    title="Er du nysgjerrig om du og Capra er en match?"
+    description="Ta en titt på stillingene våre da vel!"
+    linkText="Se stillinger"
+  />
+);
---
+++ app/components/typing-text.stories.tsx
@@ -0,0 +1,26 @@
+import { TypingText } from "./typing-text";
+
+export const ViErNorgesBestePaa = () => (
+  <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl">
+    Vi er norges beste på{" "}
+    <TypingText
+      text={[
+        "AWS",
+        "selvskryt",
+        "java",
+        "vaffelspising",
+        "javascript",
+        "smash",
+        "agile",
+        "bordtennis",
+      ]}
+    />
+  </h1>
+);
+ViErNorgesBestePaa.storyName = "Vi er norges beste på";
+
+export const Test123 = () => (
+  <div className="flex flex-col gap-2">
+    <TypingText text={["test", "test2", "test3"]} />
+  </div>
+);
