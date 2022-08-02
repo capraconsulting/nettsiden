752ccedd5a1a3040b6c03e9c88e10650dcfbcb61
Camilla <camillamdalan@gmail.com>
2022-08-02 17:19:42 +0200

    :fire: Remove picosanity because it isnt needed

--- app/utils/imageBuilder.ts
+++ app/utils/imageBuilder.ts
@@ -1,8 +1,8 @@
 import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-import { picoSanityClient } from "../sanity/getPicoClient";
+import { config } from "~/sanity/config";
 
-const builder = imageUrlBuilder(picoSanityClient);
+const builder = imageUrlBuilder(config);
 
 export const urlFor = (source: SanityImageSource) => builder.image(source);
