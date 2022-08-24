7ada79a431c76cd3bbdcd2dd2d3c7a47a5183c04
Julian Jark <juliuxu@gmail.com>
2022-08-24 14:11:45 +0200

    :lipstick: style title and text component
    remove Todo, I think it's good enough for now
    but might need some more polishing later.

--- app/components/title-and-text.tsx
+++ app/components/title-and-text.tsx
@@ -11,13 +11,15 @@ export const TitleAndText = ({
   children,
 }: TitleAndTextProps) => {
   return (
-    <Todo badge title="" className="border-none outline-dashed px-0 py-0">
-      <div className="flex flex-col items-center gap-8 lg:gap-4 w-11/12 max-w-4xl text-center">
-        <TitleComponent className="font-bold text-3xl md:text-4xl lg:text-6xl">
-          {title}
-        </TitleComponent>
-        <p>{children}</p>
-      </div>
-    </Todo>
+    // <Todo badge title="" className="border-none outline-dashed px-0 py-0">
+    <div className="w-full flex flex-col items-center gap-3 lg:gap-4 text-center">
+      <TitleComponent className="font-bold text-3xl md:text-4xl lg:text-6xl text-secondary">
+        {title}
+      </TitleComponent>
+      <p className="font-light text-lg md:text-xl lg:text-2xl text-secondary">
+        {children}
+      </p>
+    </div>
+    // </Todo>
   );
 };
