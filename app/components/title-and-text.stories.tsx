b2bdfb5a9100a8e42db619db3772d0a844570195
Julian Jark <juliuxu@gmail.com>
2022-09-03 22:16:55 +0200

    :lipstick: make TitleAndTextStories center to show that the max width works

--- app/components/title-and-text.stories.tsx
+++ app/components/title-and-text.stories.tsx
@@ -1,7 +1,7 @@
 import { TitleAndText } from "./title-and-text";
 
 export const TitleAndTextStories = () => (
-  <div className="flex flex-col gap-12">
+  <div className="flex flex-col gap-12 items-center">
     <TitleAndText title="Bli en av oss!" titleAs="h2">
       Det skal være skikkelig bra å være ansatt i Capra. Vi vet nemlig at
       kick-ass ansatte er et resultat av en god arbeidsplass - og det ansvaret
