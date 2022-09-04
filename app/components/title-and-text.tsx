29f4ff165a5a12c3837b19e1fa82192ae15d04b2
Julian Jark <juliuxu@gmail.com>
2022-09-04 19:31:38 +0200

    :bug: fix max-width on TitleAndText component
    Make the root container take full width
    while the title and body have a max width.
    This way the text is centered correctly

--- app/components/title-and-text.tsx
+++ app/components/title-and-text.tsx
@@ -9,8 +9,8 @@ export const TitleAndText = ({
   children,
 }: TitleAndTextProps) => {
   return (
-    <div className="w-full max-w-7xl flex flex-col items-center gap-3 lg:gap-4 text-center">
-      <TitleComponent className="font-bold text-3xl md:text-4xl lg:text-6xl text-secondary">
+    <div className="w-full flex flex-col items-center gap-3 lg:gap-4 text-center">
+      <TitleComponent className="max-w-7xl font-bold text-3xl md:text-4xl lg:text-6xl text-secondary">
         {title}
       </TitleComponent>
       <p className="max-w-4xl font-light text-lg md:text-xl lg:text-2xl text-secondary">
