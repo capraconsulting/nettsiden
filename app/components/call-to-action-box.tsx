c05a075ccf1168ee0ab3dae301e38dfe81ff77fb
Julian Jark <juliuxu@gmail.com>
2022-11-04 16:03:15 +0100

    :recycle: remove duplicate tailwind classes

--- app/components/call-to-action-box.tsx
+++ app/components/call-to-action-box.tsx
@@ -14,7 +14,7 @@ export const CallToActionBox = ({
   href,
 }: CallToActionBoxProps) => {
   return (
-    <div className="w-11/12 max-w-7xl py-24 px-6 bg-peach-20 flex flex-col items-center flex flex-col gap-7 text-secondary">
+    <div className="w-11/12 max-w-7xl py-24 px-6 bg-peach-20 flex flex-col items-center gap-7 text-secondary">
       <div className="text-center flex flex-col gap-1">
         <p className="font-bold text-xl">{title}</p>
         <p>{description}</p>
