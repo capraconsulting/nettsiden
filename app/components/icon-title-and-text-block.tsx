82d1b82ecd149bb7e0ade37ec52e1843800e6650
Julian Jark <juliuxu@gmail.com>
2022-11-09 10:08:38 +0100

    :recycle: make h3 come in as a explicit prop
    same as `title-and-text` component
    
    headings should be in correct order, and this component might be used directly under a h1, or even under a h3, we don't know
    
    I think it's better to leave it to the user of the component
    
    also makes it clearer when looking at the page code
    
    https://www.w3.org/WAI/tutorials/page-structure/headings/

--- app/components/icon-title-and-text-block.tsx
+++ app/components/icon-title-and-text-block.tsx
@@ -3,17 +3,20 @@ import React from "react";
 
 type IconTitleAndTextBlockProps = {
   title: string;
+  titleAs: "h1" | "h2" | "h3" | "h4";
   img: string;
   imgAlt: string;
 };
 
 const IconTitleAndTextBlock: React.FC<
   PropsWithChildren<IconTitleAndTextBlockProps>
-> = ({ title, children, img, imgAlt }) => {
+> = ({ title, titleAs: TitleComponent, children, img, imgAlt }) => {
   return (
     <div className="flex flex-col gap-3 text-center">
       <img src={img} alt={imgAlt} className="max-h-28 sm:max-h-32" />
-      <h3 className="text-2xl font-bold md:text-3xl">{title}</h3>
+      <TitleComponent className="text-2xl font-bold md:text-3xl">
+        {title}
+      </TitleComponent>
       <p>{children}</p>
     </div>
   );
