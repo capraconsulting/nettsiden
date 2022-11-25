3ce78c763d393c0050ae99f242b8aa2708497a9b
Julian Jark <juliuxu@gmail.com>
2022-11-25 16:55:26 +0100

    typeof

--- app/routes/test/cached-fetch.ts
+++ app/routes/test/cached-fetch.ts
@@ -6,11 +6,11 @@ export const loader = async ({ request }: LoaderArgs) => {
   const cacheTtl = Number.parseInt(searchParams.get("cacheTtl") ?? "") || 10;
   const purge = searchParams.get("purge") === "true";
 
-  const defaultCache = caches.default;
+  const defaultCache = (caches as any).default as Cache;
   const f = await caches.open("");
-  const cacheKeys = f?.keys?.();
-  const defaultCacheKeys = defaultCache?.keys?.();
-
+  const cacheKeys = await f?.keys?.();
+  const defaultCacheKeys = await defaultCache?.keys?.();
+  const keysFunction = typeof defaultCache?.keys;
   const catFact = await fetch("https://catfact.ninja/fact?test=123", {
     cf: {
       cacheEverything: true,
@@ -24,6 +24,8 @@ export const loader = async ({ request }: LoaderArgs) => {
     cacheKeys,
     defaultCache,
     defaultCacheKeys,
+    keysFunction,
+
     purged: purge,
     cacheTtl,
     now: new Date(),
