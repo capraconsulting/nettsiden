112dd7a71fc376e3cc34f178b619bbe034da9dad
Julian Jark <juliuxu@gmail.com>
2022-11-26 18:19:51 +0100

    :wheelchair: add support for heading in call-to-action-box
    makes it easier for screen reader users to jump to the call to action

--- app/components/call-to-action-box.tsx
+++ app/components/call-to-action-box.tsx
@@ -2,6 +2,7 @@ import { Button } from "~/components/button";
 
 interface CallToActionBoxProps {
   title: React.ReactNode;
+  titleAs?: "h2" | "h3" | "h4" | "p";
   description: React.ReactNode;
   linkText: React.ReactNode;
   href: string;
@@ -9,6 +10,7 @@ interface CallToActionBoxProps {
 
 export const CallToActionBox = ({
   title,
+  titleAs: TitleComponent = "p",
   description,
   linkText,
   href,
@@ -16,7 +18,7 @@ export const CallToActionBox = ({
   return (
     <div className="relative flex w-screen max-w-7xl flex-col items-center gap-7 bg-peach-20 py-24 px-6 text-secondary lg:w-11/12">
       <div className="flex flex-col gap-1 text-center">
-        <p className="text-xl font-bold">{title}</p>
+        <TitleComponent className="text-xl font-bold">{title}</TitleComponent>
         <p>{description}</p>
       </div>
       <Button variant="solid" href={href}>
